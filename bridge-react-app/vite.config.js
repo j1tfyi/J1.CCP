04cd05284dedf326de9d084b9e8c082524697dcf
El Gringo MD <137904081+WhiskeySic@users.noreply.github.com>
2025-01-23 17:49:57 -0500

    Update vite.config.js

--- bridge-react-app/vite.config.js
+++ bridge-react-app/vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
     rollupOptions: {
-      input: "/index.html",
+      input: "./index.html",
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
     },
   },
 });
